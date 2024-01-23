bc8b4363f25dfb98183650a553183048b1b78874
Nathan Scully <nathan.r.scully@gmail.com>
2024-01-23 15:46:16 +1100

    Added pnpm2nix build

--- flake.nix
+++ flake.nix
@@ -4,17 +4,31 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    pnpm2nix.url = "github:nathanscully/pnpm2nix-nzbr";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, pnpm2nix }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
+      let
+        pkgs = import nixpkgs { inherit system; };
+        mkPnpmPackage = pnpm2nix.packages.${system}.mkPnpmPackage;
       in with pkgs; rec {
+        server = mkPnpmPackage {
+          src = ./.;
+          installInPlace = true;
+          script = "deploy-server";
+          distDir = "deploy-server";
+        };
+
         # Development environment
         devShell = mkShell {
           name = "pnpm-monorepo-test";
-          nativeBuildInputs = [ nodejs typescript nodePackages.pnpm  ];
+          nativeBuildInputs = [ nodejs typescript nodePackages.pnpm ];
         };
-      }
-    );
+
+        packages = {
+          inherit server;
+          default = server;
+        };
+      });
 }
