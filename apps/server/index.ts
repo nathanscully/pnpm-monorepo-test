67bcfa6ab5a73c1224bb756f897446edfb7909d6
Nathan Scully <nathan.r.scully@gmail.com>
2024-05-16 15:14:39 +1000

    feat: biome + clean up

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -1,22 +1,25 @@
-import fastify, { FastifyInstance, FastifyRequest, FastifyReply } from 'fastify';
-import add from 'adder';
+import add from "adder";
+import fastify, {
+  FastifyInstance,
+  type FastifyRequest,
+  FastifyReply,
+} from "fastify";
 
-const server = fastify()
+const server = fastify();
 
-server.get('/ping', async (request: FastifyRequest, reply) => {
-    return 'pong\n'
-})
-
-server.get('/adder', async (request:any, reply:any) => {
-    const { a, b } = request.query;
-    return add(parseInt(a),parseInt(b));
+server.get("/ping", async (request: FastifyRequest, reply) => {
+  return "pong\n";
 });
 
+server.get("/adder", async (request: any, reply: any) => {
+  const { a, b } = request.query;
+  return add(Number.parseInt(a), Number.parseInt(b));
+});
 
-server.listen({ port: 8080, host: '::'}, (err, address) => {
-    if (err) {
-        console.error(err)
-        process.exit(1)
-    }
-    console.log(`Server listening at ${address}`)
-})
+server.listen({ port: 8080, host: "::" }, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Server listening at ${address}`);
+});
