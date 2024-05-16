bbe2928c5937d6cc8de2e0d25ca300377f380ab0
Nathan Scully <nathan.r.scully@gmail.com>
2024-05-16 14:59:10 +1000

    feat: pnpm bump

--- flake.nix
+++ flake.nix
@@ -4,16 +4,40 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    nathanscully.url = "github:nathanscully/nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
-      in with pkgs; rec {
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      nathanscully,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlay = final: prev: {
+          # Inherit the changes into the overlay
+          inherit (nathanscully.legacyPackages.${system}) nodePackages;
+        };
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ overlay ];
+        };
+      in
+      with pkgs;
+      rec {
         # Development environment
         devShell = mkShell {
           name = "pnpm-monorepo-test";
-          nativeBuildInputs = [ nodejs typescript nodePackages.pnpm turbo  ];
+          nativeBuildInputs = [
+            nodejs
+            typescript
+            nodePackages.pnpm
+            turbo
+            nixpacks
+          ];
         };
       }
     );
